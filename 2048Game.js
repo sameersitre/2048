9af6fb646f166ba1eec54de58dc37d8329577ae0
Sameer <sameersitre@gmail.com>
2018-09-05 22:02:57 +0530

    right shift of variables  completed

--- 2048Game.js
+++ 2048Game.js
@@ -9,108 +9,117 @@ var matrix = [
 
 var score = 0;
 var random = 0;
-var emptyElementsIndexArray = []
+var emptyElementsArray = []
 const readline = require('readline');
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
 getInput();
 
 function getInput() {
-    
+
     displayGame();
     rl.question('UP(W) DOWN(S) LEFT(A) RIGHT(D) QUIT(Q)', answer => {
         enteredKey(answer);
     });
 }
 
+/* -------------------KEY ENTERED CONDITIONS-------------------- */
 function enteredKey(answer) {
+
     if (answer === 'a') {
         console.log('Shifted Left', answer);
+        leftShifter();
+        getInput();
     }
     else if (answer === 's') {
         console.log('Shifted Down', answer);
+        downShifter();
+        getInput();
     }
     else if (answer === 'd') {
         console.log('Shifted Right', answer);
         rightShifter();
-
+        addTwoinEmptyElements();
         getInput();
     }
     else if (answer === 'w') {
         console.log('Shifted Top', answer);
+        topShifter();
+        getInput();
     }
     else if (answer === 'q') {
         console.log('Game Quitted');
         rl.close();
-
     }
     else {
         console.log('Wrong key pressed');
         getInput();
     }
-
 }
+/* ------------------DISPLAY------------------- */
 function displayGame() {
 
     console.log("**** 2 0 4 8 ****\n\nScore : " + score + "\n\n")
-    for (var i = 0; i < 4; i++) {
+    for (let i = 0; i < 4; i++) {
         console.log(matrix[i][0] + "\t" + matrix[i][1] + "\t" + matrix[i][2] + "\t" + matrix[i][3] + "\t\n")
     }
 }
 
-//addTwoinEmptyElements();
+
 function addTwoinEmptyElements() {
     //find empty elements and put into variable array
-    for (var i = 0; i < 4; i++) {
-        for (var j = 0; j < 4; j++) {
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
             if (matrix[i][j] == 0) {
-                emptyElementsIndexArray.push(i.toString() + j.toString())
+                emptyElementsArray.push(i.toString() + j.toString())
             }
         }
     }
-    console.log(emptyElementsIndexArray);
-    //getting random element from emptyElementsIndexArray
+    console.log(emptyElementsArray);
+    /* empty array->  emptyElementsArray= ['00', '01', '02', '10', '11', '20', '21', '30', '31', '32', '33' ] */
+    //getting two random element from array emptyElementsArray
+  
 
-    var x = emptyElementsIndexArray[Math.floor(Math.random() * emptyElementsIndexArray.length)]
-    //now, adding 2 in matrix of random index 
-    matrix[x.charAt(0)][x.charAt(1)] = 2;
+        let x = emptyElementsArray[Math.floor(Math.random() * emptyElementsArray.length)];
+        let y = emptyElementsArray[Math.floor(Math.random() * emptyElementsArray.length)];
+
+        //now, adding 2 in matrix of random index 
+       
+            matrix[x.charAt(0)][x.charAt(1)] = 2;
+            matrix[y.charAt(0)][y.charAt(1)] = 2;
+
+    
+
 
 }
 
+/* -------------------HORIZONTAL EQUAL NOS SUM + shift right-------------------- */
 function rightShifter() {
-    for (var i = 0; i < 4; i++) {
-        for (var j = 0; j < 4; j++) {
+
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
             if (matrix[i][j] == matrix[i][j + 1]) {
                 //if left & right elements equal, right element multiplied by itself, & left element assigned to 0
                 matrix[i][j + 1] *= 2
                 matrix[i][j] = 0;
-
-                //if right element is 0 then left element is assigned to it and left assigned to 0
-                // if (matrix[i][j + 1] == 0) {
-                //     matrix[i][j + 1] = matrix[i][j]
-                //     matrix[i][j] = 0
-                // }
             }
         }
     }
 
-    for (i = 0; i < 4; i++) {
-        for (j = 3; j >= 0; j--) {
-            if (matrix[i][j] == 0) {
+    //SHIFT ZEROES IN THE LEFT
+    for (let i = 0; i < 4; i++) {
+        for (let k = 0; k < 4; k++) {
+            for (let j = 4; j > 0; j--) {
+                if (matrix[i][j] === 0 && matrix[i][j - 1] != 0) {
+                    matrix[i][j] = matrix[i][j - 1];
+                    matrix[i][j - 1] = 0;
+                }
             }
         }
     }
-    // displayGame();
-    // for (var i = 4; i > 0; i--) {
-    //     for (var j = 4; j > 0; j--) {
-    //         if (matrix[i][j] == 0 ) {
-    //             matrix[i][j] = matrix[i][j - 1]
-    //             matrix[i][j - 1] = 0
-    //         }
-    //     }
-    // }
 }
 
 function leftShifter() {
