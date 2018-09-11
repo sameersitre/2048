e0876f946f4e430d37470acd98f5b6abb3f28b3d
Sameer <sameersitre@gmail.com>
2018-09-11 23:41:25 +0530

    minor changes

--- 2048-react/src/App.js
+++ 2048-react/src/App.js
@@ -2,17 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
+ 
   render() {
+     const Box = posed.div({
+      left: { x: -100 },
+      right: { x: 100 }
+    })
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+
+
+          const Component = ({position}) =>
+  <Box pose={position} />
+        </div>
       </div>
     );
   }
